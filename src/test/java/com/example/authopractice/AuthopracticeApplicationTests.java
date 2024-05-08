b0b0b8c455a2c4e59a6729a6400ec9a226a07a53
Rajesh1120 <nrajesh1120@gmail.com>
2024-05-08 14:30:56 -0500

    oauth2-client

---
+++ src/test/java/com/example/authopractice/AuthopracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthopracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
